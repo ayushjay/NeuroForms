a0df727e9cb9f2ad1de7044ef82b09fd5f1e6eae
Ayush J <ayushjayaswal51@gmail.com>
2026-02-19 13:31:48 +0530

    auth/added user auth

--- forms_project/my_auth/urls.py
+++ forms_project/my_auth/urls.py
@@ -1,7 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from . import views 
+from .views import SignUpView
+
+
 urlpatterns = [
     path("accounts/",include('django.contrib.auth.urls')),
+    path("accounts/signup/", SignUpView.as_view(), name="signup"),
 ]
